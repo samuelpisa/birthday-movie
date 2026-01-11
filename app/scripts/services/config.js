6a111c950f8a7571dffbe7c61b9e868aa922641c
Samuel Pisa <688745+samuelpisa@users.noreply.github.com>
2026-01-11 07:18:10 -0300

    Update TMDB endpoints to HTTPS (#2)

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -10,7 +10,7 @@
 angular.module('birthdayMovieApp')
   .constant('config', {
   	key: 'ff7fa02915424ca79751e6031e46d4a3',
-  	urlMovie: 'http://api.themoviedb.org/3/',
+  	urlMovie: 'https://api.themoviedb.org/3/',
   	urlYouTube: 'https://www.youtube.com/embed/'
 
   });
